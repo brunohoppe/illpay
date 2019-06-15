97948033d8bbc73e44bdce970d6aaf8eac3f3b2f
brunohoppe <0hoppe0@gmail.com>
2019-06-15 16:43:20 -0300

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,9 @@
 const paymentRoutes = require('./payment-route');
-
+const categoryRoutes = require('./payment-route');
 module.exports = function(app, db){
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../index.html'));
     });
     paymentRoutes(app,db);
+    categoryRoutes(app,db);
 }
